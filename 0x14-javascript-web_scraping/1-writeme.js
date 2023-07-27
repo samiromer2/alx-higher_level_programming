f3b891467dbb922a9f998310cb1f1fe1d177194f
Samir Omer <samiromer2@yahoo.com>
2023-07-27 05:07:19 +0300

    a

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const file = process.argv[2];
-const string = process.argv[3];
-
-fs.writeFile(file, string, 'utf-8', function (err) {
-  if (err) console.log(err);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
