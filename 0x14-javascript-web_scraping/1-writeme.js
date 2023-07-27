d18fc2cbcec0e9d1539d862e92fed8817f8df5ef
Samir Omer <samiromer2@yahoo.com>
2023-07-27 04:50:03 +0300

    1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-// A script that writes a string to a file
 
 const fs = require('fs');
 const file = process.argv[2];
-const write_ = process.argv[3];
+const string = process.argv[3];
 
-fs.writeFile(file, write_, 'utf8', function (error) {
-  if (error) {
-    console.log(error);
-  }
+fs.writeFile(file, string, 'utf-8', function (err) {
+  if (err) console.log(err);
 });
