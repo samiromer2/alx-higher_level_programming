d88b7f6b5a29b91136277522a8151652e3e2f09d
Samir Omer <samiromer2@yahoo.com>
2023-07-27 04:44:51 +0300

    spacing

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,10 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
+fs.readFile(process.argv[2], 'utf-8', function (err, result) {
   if (err) {
     console.log(err);
   } else {
-    process.stdout.write(data);
+    console.log(result);
   }
 });
